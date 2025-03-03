7f10acce6a1c0bb40626b8397ec41ef5948ff73f
Rob Laughlin <waterfallkicks@gmail.com>
2025-03-03 09:54:02 -0800

    Add material icons to header

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
 import "../css/reset.css";
 import "../css/index.css";
+import "iconify-icon";
+
+// function injectIcons() {
+//     const iconContainer = document.querySelector("#header .iconContainer");
+//     const github = iconContainer.querySelector(".github");
+//     const linkedin = iconContainer.querySelector(".linkedin");
+//     const twitter = iconContainer.querySelector(".twitter");
+
+//     // [icon container, icon path, alt text]
+//     const icons = [
+//         [github, mdiGithub, "github"],
+//         [linkedin, mdiLinkedin, "linkedin"],
+//         [twitter, mdiTwitter, "twitter"],
+//     ];
+
+//     icons.forEach((icon) => {
+//         const [container, iconPath, alt] = icon;
+//         const iconImg = document.createElement("span");
+//         // iconImg.alt = alt;
+//         // iconImg.src = iconPath;
+//         container.appendChild(iconImg);
+//     });
+// }
+
+function main() {
+    // injectIcons();
+}
+
+main();
